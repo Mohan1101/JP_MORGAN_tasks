57c82e2d413b206c0f322ba0ea8e3086207a5f86
Mohan Raj <mohanpraveen4161@gmail.com>
2023-06-27 19:18:04 +0530

    commited

--- client3.py
+++ client3.py
@@ -15,7 +15,8 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    
+    price = (bid_price+ask_price) / 2
+
 
 
 def getRatio(price_a, price_b):
